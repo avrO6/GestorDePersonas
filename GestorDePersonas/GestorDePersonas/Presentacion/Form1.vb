c646ebb76d192b04f9c88439ab8391d27959c170
avrO6 <127057199+avrO6@users.noreply.github.com>
2023-05-22 02:02:02 +0200

    Update Form1.vb

--- GestorDePersonas/GestorDePersonas/Presentacion/Form1.vb
+++ GestorDePersonas/GestorDePersonas/Presentacion/Form1.vb
@@ -1,24 +1,45 @@
-﻿Public Class Form1
+Public Class Form1
     Private Sub añadir_Click(sender As Object, e As EventArgs) Handles añadir.Click
         If dni.Text IsNot Nothing Then
             Dim newPersona As New Persona
             Dim selPersona As New Persona
-            selPersona.leerPersona(txt_DNI.Text)
-            If dni.Text < AcceptButton selPersona.DNI Then
-                newPersona.DNI = dni.Text
-                newPersona.nombre = dni.Text
+            selPersona.leerpersona(dni.Text)
+            If dni.Text <> selPersona.NDNI1 Then
+                newPersona.NDNI1 = dni.Text
+                newPersona.NOM = nombre.Text
                 newPersona.insertarPersona()
-                AccessibleDefaultActionDescription()
-                MessageBox.Show("Se ha añadido la persona " + newPersona.DNI + " satisfactoriamente")
+                Actualizar()
+                MessageBox.Show("Se ha añadido la persona " + newPersona.NDNI1 + " satisfactoriamente")
             End If
+        End If
     End Sub
 
     Private Sub modificar_Click(sender As Object, e As EventArgs) Handles modificar.Click
-
+        If dni.Text IsNot Nothing Then
+            Dim newPersona As New Persona
+            Dim selPersona As New Persona
+            selPersona.leerpersona(dni.Text)
+            If dni.Text <> selPersona.NDNI1 Then
+                newPersona.NDNI1 = dni.Text
+                newPersona.NOM = dni.Text
+                newPersona.modificarPersona()
+                MessageBox.Show("Se ha modificado la persona " + newPersona.NDNI1 + " satisfactoriamente")
+            End If
+        End If
     End Sub
 
     Private Sub eliminar_Click(sender As Object, e As EventArgs) Handles eliminar.Click
-
+        If dni.Text IsNot Nothing Then
+            Dim newPersona As New Persona
+            Dim selPersona As New Persona
+            selPersona.leerpersona(dni.Text)
+            If dni.Text <> selPersona.NDNI1 Then
+                newPersona.NDNI1 = dni.Text
+                newPersona.NOM = dni.Text
+                newPersona.eliminarPersona()
+                MessageBox.Show("Se ha eliminado la persona " + newPersona.NDNI1 + " satisfactoriamente")
+            End If
+        End If
     End Sub
 
     Private Sub limpiar_Click(sender As Object, e As EventArgs) Handles limpiar.Click
@@ -35,6 +56,10 @@
     End Sub
 
     Private Sub borrarBBDD_Click(sender As Object, e As EventArgs) Handles borrarBBDD.Click
+
+    End Sub
+
+    Sub Actualizar()
 
     End Sub
 End Class
